f7508b864d622f9ec87932c29832102249983a7f
mood1nthemooon <md1ntmn@gmail.com>
2024-05-30 16:34:39 +0300

    Добавлены анимации

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,40 @@
 import '../scss/style.scss';
 import 'bootstrap/js/dist/collapse';
 
+const fadeIn = [
+    [
+        { opacity: 0, scale: 0.9 },
+        { opacity: 1, scale: 1 }
+    ],
+    {
+        duration: 300,
+        iterations: 1,
+        fill: 'forwards'
+    } 
+];
+
+const observer = new IntersectionObserver((entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            for (let index = 0; index < entry.target.children.length; index++) {
+                const child = entry.target.children[index];
+                const animation = child.animate(...fadeIn);    
+                animation.pause();
+                
+                setTimeout(() => {
+                    animation.play();
+                }, 100 * index);
+
+                // animation.finished.then(() => {
+                //     animation.commitStyles();
+                // });
+            }
+        }
+    }
+});
+
+const photos = document.querySelectorAll('[photos]');
+
+photos.forEach((photo) => {
+    observer.observe(photo);
+});
